aa7f595b51d4a451f548bcb18016ec7d518297f6
MJ-Dev92 <minjae920802@naver.com>
2023-01-10 06:40:43 +0900

    소셜로그인 카카오 완성

---
+++ src/api/login.js
@@ -0,0 +1,13 @@
+const {
+  REACT_APP_KAKAO_REST_API_KEY,
+  REACT_APP_KAKAO_REDIRECT_URI,
+  REACT_APP_GOOGLE_CLIENT_ID,
+  REACT_APP_GOOGLE_REDIRECT_URI,
+} = process.env;
+
+export const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${REACT_APP_GOOGLE_CLIENT_ID}&redirect_uri=${REACT_APP_GOOGLE_REDIRECT_URI}&response_type=code&scope=profile%20email
+  `;
+
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REACT_APP_KAKAO_REST_API_KEY}&redirect_uri=${REACT_APP_KAKAO_REDIRECT_URI}&response_type=code`;
+
+export const GITHUB_AUTH_URL = `https://github.com/login/oauth/authorize?client_id=d83af89cb38b172425e9&redirect_uri=http://localhost:8080/login/oauth2/code/github`;
---
+++ src/components/User/Signin.jsx
@@ -0,0 +1,84 @@
+import { useNavigate } from "react-router-dom";
+import styled, { css } from "styled-components";
+// import { useInput } from "../../../lib/utils/useInput";
+// import { __postLogin } from "../../../redux/modules/loginSlice";
+import {
+  GOOGLE_AUTH_URL,
+  KAKAO_AUTH_URL,
+  GITHUB_AUTH_URL,
+} from "../../api/login";
+import { Link } from "react-router-dom";
+// import { LockClosedIcon } from "@heroicons/react/20/solid";
+
+// LOGIN
+const Login = () => {
+  const onClickHandler = (flag) => {
+    if (flag === "kakao") {
+      window.location.href = KAKAO_AUTH_URL;
+    } else if (flag === "github") {
+      window.location.href = GITHUB_AUTH_URL;
+    } else if (flag === "google") {
+      window.location.href = GOOGLE_AUTH_URL;
+    }
+  };
+  return (
+    <>
+      {/*
+        This example requires updating your template:
+
+        ```
+        <html class="h-full bg-gray-50">
+        <body class="h-full">
+        ```
+      */}
+      <div className="flex min-h-full items-center justify-center py-12">
+        <div className="w-full max-w-md h-full space-y-20 mt-40">
+          <div>
+            <img
+              className="mx-auto h-32 w-42"
+              src="./img/home_logo.png"
+              alt="Your Company"
+            />
+            <h2 class="mt-6 text-center text-l font-bold tracking-tight text-gray-900">
+              갓생 사는 개발자를 위한 소셜 데이팅 앱
+            </h2>
+          </div>
+
+          <div class="space-y-4 flex flex-col items-center justify-center">
+            <button
+              className="group relative flex w-80 h-12 justify-center items-center rounded-md border border-transparent bg-[#EFDB30] py-2 px-4 text-sm font-medium text-[#000000] focus:outline-none focus:ring-2  focus:ring-offset-2"
+              onClick={() => onClickHandler("kakao")}
+            >
+              <img
+                className="mr-2"
+                src="./img/kakaoTalk.png"
+                alt="KakaotalkLogo Logo"
+              ></img>
+              카카오 로그인
+            </button>
+            <button
+              className="group relative flex w-80 h-12 justify-center items-center rounded-md border border-transparent bg-[#FFFFFF] py-2 px-4 text-sm font-medium text-black focus:outline-none focus:ring-2  focus:ring-offset-2 border border-[#000000]"
+              onClick={() => onClickHandler("google")}
+            >
+              <img src="./img/Logo.png" alt="Google Logo"></img>
+              Google로 로그인
+            </button>
+            <button
+              className="group relative flex w-80 h-12 justify-center items-center rounded-md border border-transparent bg-[#222222] py-2 px-4 text-sm font-medium text-white focus:outline-none focus:ring-2  focus:ring-offset-2"
+              onClick={() => onClickHandler("github")}
+            >
+              <img
+                className="mr-2"
+                src="./img/Github.png"
+                alt="Github Logo"
+              ></img>
+              Github으로 계속하기
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;

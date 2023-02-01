e9655f8bb547a9435924c8a346e4ec282c8f2582
MJ-Dev92 <minjae920802@naver.com>
2023-02-01 22:07:58 +0900

    기본틀 수정 그림자 추가

--- src/components/likeme/LikeMe.jsx
+++ src/components/likeme/LikeMe.jsx
@@ -36,50 +36,53 @@ const LikeMe = () => {
 
   return (
     <div className="flex items-center justify-center">
-      <div className="ml-8">
-        <div className="w-96 flex flex-wrap ">
-          <div className="flex flex-col mt-[86px]">
-            <div className="flex flex-row mb-[40px] ">
-              <img
-                src="/img/likelogo.png"
-                alt="likelogo"
-                className="w-[32px] h-[30px] mr-2"
-              />
-              <p className="text-[#000] text-2xl font-bold ">받은 좋아요</p>
-            </div>
-            {data.map((post, index) => (
-              <div className="w-[390px] h-[130px] ml-[-15px] border-b-[2px] flex items-center ">
-                <div className="flex flex-row ml-[10px]">
-                  <img
-                    src="/img/person1.png"
-                    alt="person1"
-                    className="w-[90px] h-[90px] rounded-[10px]"
-                  />
-                  <div className="flex flex-col ml-2 mb-[4px]">
-                    <div className="flex flex-row">
-                      <p className="mr-2 font-bold text-[20px]">
-                        {post.username}
-                      </p>
-                      <span className="font-normal text-[18px] mr-2">
-                        {post.age}
-                      </span>
-                    </div>
+      <div className=" shadow-xl">
+        <div className="ml-[20px]">
+          <div className="w-[372px] flex flex-wrap ">
+            <div className="flex flex-col mt-[40px]">
+              <div className="flex flex-row mb-[40px] ">
+                <img
+                  src="/img/likelogo.png"
+                  alt="likelogo"
+                  className="w-[32px] h-[30px] mr-2"
+                />
+                <p className="text-[#000] text-2xl font-bold ">받은 좋아요</p>
+              </div>
+              {data.data.profileList.map((post, index) => (
+                <div className="w-[370px] h-[130px] ml-[-10px] border-b-[2px] flex items-center ">
+                  <div className="flex flex-row ml-[10px]">
+                    <div
+                      style={{
+                        backgroundImage: `url(${post.imageList[0]})`,
+                      }}
+                      className="w-[90px] h-[90px] rounded-[10px] bg-cover"
+                    />
+                    <div className="flex flex-col ml-2 mb-[4px]">
+                      <div className="flex flex-row">
+                        <p className="mr-2 font-bold text-[20px]">
+                          {post.username}
+                        </p>
+                        <span className="font-normal text-[18px] mr-2">
+                          {post.age}
+                        </span>
+                      </div>
 
-                    <p className="text-[14px]">
-                      {post.job} . {post.residence}
-                    </p>
-                    <div className="flex flex-row mt-[5px]">
-                      <button className="w-[90px] h-[32px] bg-[#28CC9E] text-[#fff] rounded-[8px] mr-[8px] hover:bg-[#fff] border hover:border-[#28CC9E] hover:text-[#28CC9E] duration-300">
-                        <p>연결하기</p>
-                      </button>
-                      <button className="w-[90px] h-[32px] rounded-[8px] border border-gray text-[#000] hover:bg-[grey] hover:text-[#fff] duration-300">
-                        <p>관심 없음</p>
-                      </button>
+                      <p className="text-[14px]">
+                        {post.job} . {post.residence}
+                      </p>
+                      <div className="flex flex-row mt-[5px]">
+                        <button className="w-[90px] h-[32px] bg-[#28CC9E] text-[#fff] rounded-[8px] mr-[8px] hover:bg-[#fff] border hover:border-[#28CC9E] hover:text-[#28CC9E] duration-300">
+                          <p>연결하기</p>
+                        </button>
+                        <button className="w-[90px] h-[32px] rounded-[8px] border border-gray text-[#000] hover:bg-[grey] hover:text-[#fff] duration-300">
+                          <p>관심 없음</p>
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
       </div>
--- src/components/recommend/AddRecommend.jsx
+++ src/components/recommend/AddRecommend.jsx
@@ -49,94 +49,100 @@ const AddRecommend = () => {
   };
 
   return (
-    <div className="font-SUIT flex items-center justify-center py-5">
-      <div className="ml-5">
-        <div className="w-96 flex lex items-center justify-center flex-wrap">
-          <div className="flex flex-col mt-10">
-            <a
-              href="/recommend"
-              className=" text-[#000] text-2xl font-bold ml-[-18px]"
-            >
-              <img src="/img/backarrowlogo.png" alt="BackarrowLogo" />
-            </a>
-            <p className="text-[#333] text-2xl font-bold mt-[20px]">
-              취향 설정
-            </p>
-            <p className="text-[#333] text-xl font-bold mt-[40px]">지역 설정</p>
-            <p className="text-[#333] text-[12px] font-normal mt-[8px]">
-              지역은 구 단위로 최소 1개 이상 최대 2개까지 설정할 수 있어요
-            </p>
+    <div className="font-SUIT flex items-center justify-center">
+      <div className="shadow-xl pb-10">
+        <div className="ml-5">
+          <div className="w-96 flex lex items-center justify-center flex-wrap">
+            <div className="flex flex-col mt-10">
+              <a
+                href="/recommend"
+                className=" text-[#000] text-2xl font-bold ml-[-18px]"
+              >
+                <img src="/img/backarrowlogo.png" alt="BackarrowLogo" />
+              </a>
+              <p className="text-[#333] text-2xl font-bold mt-[20px]">
+                취향 설정
+              </p>
+              <p className="text-[#333] text-xl font-bold mt-[40px]">
+                지역 설정
+              </p>
+              <p className="text-[#333] text-[12px] font-normal mt-[8px]">
+                지역은 구 단위로 최소 1개 이상 최대 2개까지 설정할 수 있어요
+              </p>
 
-            <div className="space-x-[10px]">
-              <select
-                value={residence}
-                onChange={(e) => setResidence(e.target.value)}
-                type="text"
-                className="w-[350px] h-[40px] border border-[#eee] rounded-[8px] mt-3 font-[14px] pl-3 bg-[#eee]"
-              >
-                <option value="" disabled selected hidden>
-                  지역 선택
-                </option>
-                <option value="서울">서울</option>
-                <option value="서울">경기</option>
-                <option value="경북">경북</option>
-                <option value="경남">경남</option>
-                <option value="전북">전북</option>
-                <option value="전남">전남</option>
-                <option value="충북">충북</option>
-                <option value="충남">충남</option>
-                <option value="강원">강원</option>
-                <option value="제주">제주</option>
-                <option value="인천">인천</option>
-              </select>
-            </div>
-            <p className="text-[#333] text-xl font-bold mt-[40px]">나이 설정</p>
-            <div>
-              <select
-                value={age}
-                onChange={(e) => setAge(e.target.value)}
-                type="text"
-                className="w-[350px] h-[40px] border border-[#eee] rounded-[8px] mt-3 font-[14px] pl-3 bg-[#eee]"
-              >
-                <option value="" disabled selected hidden>
-                  나이대 선택
-                </option>
-                <option value="20-24세">20-24세</option>
-                <option value="25-29세">25-29세</option>
-                <option value="30-34세">30-34세</option>
-                <option value="35-39세">35-39세</option>
-              </select>
-            </div>
-            <p className="text-[#333] text-xl font-bold mt-[40px]">
-              선호 기술스택
-            </p>
-            <p className="text-[#333] text-[12px] font-normal mt-[8px]">
-              선호 개발 언어를 5개 미만으로 선택해주세요.
-            </p>
-            <div>
-              <ul className="flex text-[#555] left-5 text-xs flex-wrap">
-                {datas.map((data, index) => (
-                  <StackCard
-                    key={index}
-                    data={data.title}
-                    checkedItem={checkedItems}
-                    checkedItemHandler={checkedItemHandler}
-                  />
-                ))}
-              </ul>
-              <button
-                className="flex w-[350px] h-[48px] justify-center items-center rounded-md border hover:border-[#28CC9E] text-[#fff] font-bold bg-[#28CC9E] hover:bg-[#fff] hover:text-[#28CC9E] duration-300 mt-[80px]"
-                onClick={() => {
-                  alert("설정 완료");
-                  mutation.mutate({
-                    age: age,
-                    residence: residence,
-                    stack: checkedItems,
-                  });
-                }}
-              >
-                <p>취향 설정하기</p>
-              </button>
+              <div className="space-x-[10px]">
+                <select
+                  value={residence}
+                  onChange={(e) => setResidence(e.target.value)}
+                  type="text"
+                  className="w-[350px] h-[40px] border border-[#eee] rounded-[8px] mt-3 font-[14px] pl-3 bg-[#eee]"
+                >
+                  <option value="" disabled selected hidden>
+                    지역 선택
+                  </option>
+                  <option value="서울">서울</option>
+                  <option value="서울">경기</option>
+                  <option value="경북">경북</option>
+                  <option value="경남">경남</option>
+                  <option value="전북">전북</option>
+                  <option value="전남">전남</option>
+                  <option value="충북">충북</option>
+                  <option value="충남">충남</option>
+                  <option value="강원">강원</option>
+                  <option value="제주">제주</option>
+                  <option value="인천">인천</option>
+                </select>
+              </div>
+              <p className="text-[#333] text-xl font-bold mt-[40px]">
+                나이 설정
+              </p>
+              <div>
+                <select
+                  value={age}
+                  onChange={(e) => setAge(e.target.value)}
+                  type="text"
+                  className="w-[350px] h-[40px] border border-[#eee] rounded-[8px] mt-3 font-[14px] pl-3 bg-[#eee]"
+                >
+                  <option value="" disabled selected hidden>
+                    나이대 선택
+                  </option>
+                  <option value="20-24세">20-24세</option>
+                  <option value="25-29세">25-29세</option>
+                  <option value="30-34세">30-34세</option>
+                  <option value="35-39세">35-39세</option>
+                </select>
+              </div>
+              <p className="text-[#333] text-xl font-bold mt-[40px]">
+                선호 기술스택
+              </p>
+              <p className="text-[#333] text-[12px] font-normal mt-[8px]">
+                선호 개발 언어를 5개 미만으로 선택해주세요.
+              </p>
+              <div>
+                <ul className="flex text-[#555] left-5 text-xs flex-wrap">
+                  {datas.map((data, index) => (
+                    <StackCard
+                      key={index}
+                      data={data.title}
+                      checkedItem={checkedItems}
+                      checkedItemHandler={checkedItemHandler}
+                    />
+                  ))}
+                </ul>
+                <button
+                  className="flex w-[350px] h-[48px] justify-center items-center rounded-md border hover:border-[#28CC9E] text-[#fff] font-bold bg-[#28CC9E] hover:bg-[#fff] hover:text-[#28CC9E] duration-300 mt-[80px]"
+                  onClick={() => {
+                    alert("설정 완료");
+                    mutation.mutate({
+                      age: age,
+                      residence: residence,
+                      stack: checkedItems,
+                    });
+                  }}
+                >
+                  <p>취향 설정하기</p>
+                </button>
+              </div>
             </div>
           </div>
         </div>
